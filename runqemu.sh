6d05997882ab21f3869ba9614b7d4060c1d7a0b0
stealthadmin <stealthadmin@Ubuntu.myguest.virtualbox.org>
2025-09-28 23:50:18 -0600

    Local Test works perfectly. Runner fails

--- runqemu.sh
+++ runqemu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script to run QEMU for buildroot as the default configuration qemu_aarch64_virt_defconfig
 #Host forwarding: Host Port 10022 ->> QEMU Port 22 and Host Port 9000 ->> QEMU Port 9000
-#Author: Siddhant Jajoo.
+#Author: Siddhant Jajoo. Modified by Donavon Facey
 
 
 qemu-system-aarch64 \

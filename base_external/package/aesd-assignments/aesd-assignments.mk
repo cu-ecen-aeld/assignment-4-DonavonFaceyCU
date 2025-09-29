6d8a6d83bf3467f3f568b4e35d38871a86a2ac86
stealthadmin <stealthadmin@Ubuntu.myguest.virtualbox.org>
2025-09-28 23:31:47 -0600

    Updated to run aesdsocket as Daemon

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 8fe1771807a52b429b7fd0bb071b9e181c21dbcf
+AESD_ASSIGNMENTS_VERSION = 75650c69a8f02a6901513d694db13f867d432714
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:

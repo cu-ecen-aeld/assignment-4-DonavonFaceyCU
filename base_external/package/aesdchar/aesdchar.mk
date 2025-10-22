c35482193af451d75e36305933db60c4b8039746
DonavonFaceyCU <dofa5688@colorado.edu>
2025-10-21 19:00:39 -0600

    Assignment 8 Finished

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,15 +6,15 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = 291a8eb4ed848f479d0726d46283bc89247e6621
+AESDCHAR_VERSION = bf0f730ee5ecc2156c8efc0c14a1fb7145980235
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignment-3-DonavonFaceyCU.git
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-DonavonFaceyCU.git
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
 
-AESDCHAR_MODULE_SUBDIRS = ased-char-driver
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 AESDCHAR_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 #TODO: Fix
@@ -26,10 +26,10 @@ AESDCHAR_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 #endef
 
 # TODO: Fix
-define LDD_INSTALL_TARGET_CMDS
-
+define AESDCHAR_INSTALL_TARGET_CMDS
+	echo "Placing AESDCHAR FILES"
 	$(INSTALL) -d 0755 $(BR2_EXTERNAL)/rootfs_overlay/etc/init.d/
-	$(INSTALL) -m 0755 $(@D)/init $(BR2_EXTERNAL)/rootfs_overlay/etc/init.d/S97aesdcharmodule
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/init $(BR2_EXTERNAL)/rootfs_overlay/etc/init.d/S97aesdcharmodule
 	
 	$(INSTALL) -d 0755 $(BR2_EXTERNAL)/rootfs_overlay/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(BR2_EXTERNAL)/rootfs_overlay/bin/
